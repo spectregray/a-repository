d1b2be1f8de33cf8bf741d2035329f4c0d8bedae
selena <appleselena@gmail.com>
2023-11-28 18:59:25 -0500

    day3p2

--- src/day3/day-3.go
+++ src/day3/day-3.go
@@ -37,8 +37,55 @@ func part1(scanner *bufio.Scanner) (int) {
 	return prioritySum
 }
 
+// assumes total no. of lines is a multiple of 3
 func part2(scanner *bufio.Scanner) (int) {
-	return 0
+	prioritySum := 0
+	sharedMap := make(map[byte]bool)
+	i := 0
+	
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// for the first elf, adds all of its bag contents to map
+		if i == 0 {
+			for j := 0; j < len(line); j++ {
+				sharedMap[line[j]] = true
+			}
+		// for i > 0, checks against the map
+		} else {
+			tempMap := make(map[byte]bool)
+			for j := 0; j < len(line); j++ {
+				tempMap[line[j]] = true
+			}
+
+			for key := range sharedMap {
+				_, ok := tempMap[key]
+				if !ok {
+					delete(sharedMap, key)
+				} 
+			}
+		}
+
+		i += 1
+		if i == 3 {
+			// there should only be one value
+			if len(sharedMap) > 1 {
+				panic("some reason, multiple values are left")
+			}
+			for key := range sharedMap {
+				ascii := int(key)
+				if ascii >= 97 {
+					prioritySum += ascii - 96
+				} else if ascii >= 65 {
+					prioritySum += ascii - 64 + 26
+				}
+			}
+			sharedMap = make(map[byte]bool)
+			i = 0
+		}
+	} 
+
+	return prioritySum
 }
 
 func main() {
